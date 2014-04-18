37549ae11b120da9f3ce8aba18f8be245c79a284
Hunter Meyer <huntermeyer@live.com>
2014-04-18 16:58:13 -0400

    Add project model and specs

---
+++ app/models/project.rb
@@ -0,0 +1,19 @@
+class Project < ActiveRecord::Base
+  before_save { demo_url.downcase! if demo_url }
+  before_save { repo_url.downcase! if repo_url }
+  before_save { check_urls if (demo_url || repo_url) }
+
+
+  def check_urls
+    if self.demo_url && self.demo_url != ''
+      unless self.demo_url.start_with?('http://') || self.demo_url.start_with?('https://')
+        self.demo_url = "http://#{self.demo_url}"
+      end
+    elsif self.repo_url && self.repo_url != ''
+      unless self.repo_url.start_with?('http://') || self.repo_url.start_with?('https://')
+        self.repo_url = "http://#{self.repo_url}"
+      end
+    end
+  end
+
+end
