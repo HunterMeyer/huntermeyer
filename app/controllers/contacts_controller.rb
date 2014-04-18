1ae236423bbba5340ce999ba6033aeb2976cb80e
Hunter Meyer <huntermeyer@live.com>
2014-04-18 17:35:37 -0400

    Add contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,22 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.create(contact_params)
+    if @contact.save
+      flash[:success] = 'Thanks you message has been sent.'
+      redirect_to root_path
+    else
+      flash[:warning] = 'Oops. Something was missing. We need your email and message.'
+      render 'new'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :content)
+  end
+end
