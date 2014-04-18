035dbc4f833fd977affb33815c6fe5ee1355edc9
Hunter Meyer <huntermeyer@live.com>
2014-04-18 12:06:00 -0400

    Update readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huntermeyer::Application.config.secret_token = '8d74fc393756beda5b2563fd56b12dc90c5bc5cb06359253fee93fbe13c8da0ee1ed318b430ed49a43fe38a87f7303c94abe4af5462e9aa3261d7f338c588364'
