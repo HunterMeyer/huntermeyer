e99f73bc4cca4249cefdb7de7f62f5f63038844a
Hunter Meyer <huntermeyer@live.com>
2014-04-18 21:18:29 -0400

    Add hosted projects routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Huntermeyer::Application.routes.draw do
   resources :contacts
-  resources :projects
+  resources :projects, except: :show
 
   root 'static_pages#index'
 
-  match '/about', to: 'static_pages#about', via: 'get'
+  match '/about',                to: 'static_pages#about',    via: 'get'
+  match 'projects/triangle',     to: 'projects#triangle',     via: 'get'
+  match 'projects/pig_latin',    to: 'projects#pig_latin',    via: 'get'
+  match 'projects/youtv',        to: 'projects#youtv',        via: 'get'
+  match 'projects/todo',         to: 'projects#todo',         via: 'get'
+  match 'projects/legislator',   to: 'projects#legislator',   via: 'get'
+  match 'projects/creditcard',   to: 'projects#creditcard',   via: 'get'
+  match 'projects/music_search', to: 'projects#music_search', via: 'get'
 end
