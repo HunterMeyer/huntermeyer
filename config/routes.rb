46e22b2d9341a8f181f52419d154c42ffdfc458d
Hunter Meyer <huntermeyer@live.com>
2014-07-19 15:12:44 -0400

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Huntermeyer::Application.routes.draw do
-  resources :contacts, except: [ :index, :show, :edit, :update, :destroy ]
-  resources :projects, except: [ :show, :edit, :update, :destroy ]
+  resources :contacts
+  resources :projects
 
   root 'static_pages#index'
 
-  match '/about',                to: 'static_pages#about',    via: 'get'
-  match 'projects/triangle',     to: 'projects#triangle',     via: 'get'
-  match 'projects/pig_latin',    to: 'projects#pig_latin',    via: 'get'
-  match 'projects/youtv',        to: 'projects#youtv',        via: 'get'
-  match 'projects/todo',         to: 'projects#todo',         via: 'get'
-  match 'projects/legislator',   to: 'projects#legislator',   via: 'get'
-  match 'projects/creditcard',   to: 'projects#creditcard',   via: 'get'
-  match 'projects/music_search', to: 'projects#music_search', via: 'get'
+  match '/creditcard',  to: 'credit_card#index',  via: 'get'
+  match '/legislator',  to: 'legislator#index',   via: 'get'
+  match '/musicsearch', to: 'music_search#index', via: 'get'
+  match '/piglatin',    to: 'pig_latin#index',    via: 'get'
+  match '/squeak',      to: 'squeak#index',       via: 'get'
+  match '/about',       to: 'static_pages#about', via: 'get'
+  match '/todo',        to: 'todo#index',         via: 'get'
+  match '/triangle',    to: 'triangle#index',     via: 'get'
+  match '/youtv',       to: 'youtv#index',        via: 'get'
 end
