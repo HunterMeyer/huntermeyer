17980d19a5663690ebbe2d4b5192415b8dece61c
Hunter Meyer <huntermeyer@live.com>
2014-07-25 00:06:09 -0400

    Add static pages to precompile

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module Huntermeyer
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += %w(*.js squeak.css credit_card.css static_pages.css.scss)
+    config.assets.precompile += %w(*.js squeak.css credit_card.css static_pages.css)
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
