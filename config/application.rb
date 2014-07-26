48df13fa0ccb3d6ba09a0f682d63a3c6989c1c15
Hunter Meyer <huntermeyer@live.com>
2014-07-26 02:39:10 -0400

    Finish legislator page

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module Huntermeyer
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += %w(*.js squeak.css credit_card.css static_pages.css)
+    config.assets.precompile += %w(*.js squeak.css credit_card.css static_pages.css legislator.css)
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
