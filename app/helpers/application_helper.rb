867d931c2817bf7a9bf70dac911825af0bdbe87a
HunterMeyer <huntermeyer@live.com>
2015-01-15 19:19:46 -0500

    Update contact me page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
   def generate_form_errors(errors)
-    content_tag(:div, class: 'error') do
-      'Oops. Something went wrong'.html_safe +
-      content_tag(:div, class: 'wrapper') do
-        errors.full_messages.map { |message| content_tag(:div, message.gsub('Content', 'Message')) }.join.html_safe
-      end
+    content_tag(:div, class: 'error-label') do
+      "<i class='fa fa-close'></i> Oops. Something went wrong:".html_safe
+    end +
+    content_tag(:ul, class: 'error-list') do
+      errors.full_messages.map { |message| content_tag(:li, message.gsub('Content', 'Message')) }.join.html_safe
     end
   end
 
