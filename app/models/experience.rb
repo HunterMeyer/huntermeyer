2d5200a2a84c64b1d7e7ccb7cc717a7af672f12c
HunterMeyer <huntermeyer@live.com>
2015-01-17 18:32:09 -0500

    Create module with shared attribue methods

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,16 +1,5 @@
 class Experience < ActiveRecord::Base
+  include AttributeExtensions
   scope :visible, -> { where(visible: true) }
-
-  def dates
-    "#{start_date} -  #{end_date}"
-  end
-
-  def location
-    "#{city}, #{state}"
-  end
-
-  def formatted_description(long_or_short)
-    instance_eval(long_or_short).split(';').map! { |d| d.gsub(/\n/, '').strip + '.' }.split.join(' ')
-  end
 
 end
---
+++ lib/attribute_extensions.rb
@@ -0,0 +1,16 @@
+module AttributeExtensions
+
+  def dates
+    "#{start_date} -  #{end_date}"
+  end
+
+  def location
+    return city if state.blank?
+    "#{city}, #{state}"
+  end
+
+  def formatted_description(column_name)
+    instance_eval(column_name).split(';').map! { |d| d.gsub(/\n/, '').strip + '.' }.split.join(' ')
+  end
+
+end
