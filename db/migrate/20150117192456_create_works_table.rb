ba4d508d0c6c031af0762e77480043850a7e3a1e
HunterMeyer <huntermeyer@live.com>
2015-01-17 14:26:38 -0500

    Create works table

---
+++ db/migrate/20150117192456_create_works_table.rb
@@ -0,0 +1,17 @@
+class CreateWorksTable < ActiveRecord::Migration
+  def change
+    create_table :works_tables do |t|
+      t.text :name
+      t.text :caption
+      t.text :headline
+      t.text :description
+      t.text :url
+      t.text :image_url
+      t.text :repo_url
+      t.text :twitter_url
+      t.text :tags
+      t.integer :ordinal
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111215147) do
+ActiveRecord::Schema.define(version: 20150117192456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,5 +56,20 @@ ActiveRecord::Schema.define(version: 20150111215147) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "works_tables", force: true do |t|
+    t.text     "name"
+    t.text     "caption"
+    t.text     "headline"
+    t.text     "description"
+    t.text     "url"
+    t.text     "image_url"
+    t.text     "repo_url"
+    t.text     "twitter_url"
+    t.text     "tags"
+    t.integer  "ordinal"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
