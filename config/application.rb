19a8e5b7d3f9ebcaab2f321d1fcd7fb3e8b6f28a
HunterMeyer <huntermeyer@live.com>
2015-01-17 20:59:20 -0500

    Add js and css to assets precompile

--- config/application.rb
+++ config/application.rb
@@ -57,8 +57,7 @@ module Huntermeyer
     # Enable the asset pipeline
     config.assets.enabled = true
     # config.assets.precompile += %w(*.js squeak.css credit_card.css static_pages.css legislator.css)
-    config.assets.paths << "#{Rails.root}/app/assets/img"
-    # config.assets.precompile += %w(*.js squeak.css credit_card.css static_pages.css legislator.css)
+    config.assets.precompile += %w(*.js *.css)
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
