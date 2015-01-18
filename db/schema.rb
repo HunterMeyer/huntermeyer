8d6ea7bc7de90bb246d78bf69b8aee6256a2b07c
HunterMeyer <huntermeyer@live.com>
2015-01-17 19:01:18 -0500

    Create skills table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117223041) do
+ActiveRecord::Schema.define(version: 20150117233455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,15 @@ ActiveRecord::Schema.define(version: 20150117223041) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "ordinal"
+  end
+
+  create_table "skills", force: true do |t|
+    t.text     "name"
+    t.integer  "width"
+    t.integer  "ordinal"
+    t.boolean  "visible"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
