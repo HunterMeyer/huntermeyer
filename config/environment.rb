30d859c92ad0e5ff4a56353cf1e6153e7744b1c7
HunterMeyer <huntermeyer@live.com>
2016-09-10 19:29:05 -0400

    Remove old mandril crap, dont send emails in development

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Huntermeyer::Application.initialize!
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  address:              'smtp.mandrillapp.com',
-  port:                 587,
-  user_name:            'huntermeyer@live.com',
-  password:             'HKw7Y742JAUZxhNlvlxQCA',
-  authentication:       :login,
-  enable_starttls_auto: true
-}
