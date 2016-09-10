620c535b1e0ea65467e5ad76d76dae846e0f5d2e
HunterMeyer <huntermeyer@live.com>
2016-09-10 19:45:08 -0400

    Setup smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,6 @@ Huntermeyer::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries    = true
-  config.action_mailer.delivery_method       = :smtp
-  config.action_mailer.default_url_options   = { host: 'huntermeyer.com' }
 
   # Enable threaded mode
   # config.threadsafe!
