e3d0ef3b4b7633bd2100a905902d79239b6f4697
HunterMeyer <huntermeyer@live.com>
2016-09-10 19:27:25 -0400

    Update AttributeExtensions with ordinal increment/decrement

--- lib/attribute_extensions.rb
+++ lib/attribute_extensions.rb
@@ -1,16 +1,27 @@
 module AttributeExtensions
-
-  def dates
-    "#{start_date} - #{end_date}"
+  def self.included(base)
+    base.include InstanceMethods
+    base.extend ClassMethods
   end
 
-  def location
-    return city if state.blank?
-    "#{city}, #{state}"
+  module ClassMethods
+    def change_ordinals(direction = :increment)
+      all.each { |obj| obj.send("#{direction}!", :ordinal) }
+    end
   end
 
-  def formatted_description(column_name)
-    instance_eval(column_name).split(';').map! { |d| d.gsub(/\n/, '').strip + '.' }.split.join(' ')
-  end
+  module InstanceMethods
+    def dates
+      "#{start_date} - #{end_date}"
+    end
 
+    def location
+      return city if state.blank?
+      "#{city}, #{state}"
+    end
+
+    def formatted_description
+      long_description.gsub(';', '.').gsub(/\n/, '')
+    end
+  end
 end
