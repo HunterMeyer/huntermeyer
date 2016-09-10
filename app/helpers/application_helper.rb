c1e428e268f9417102d1064a60c820175df9fe96
HunterMeyer <huntermeyer@live.com>
2016-09-10 19:29:51 -0400

    Add some helper methods to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-
   def generate_form_errors(errors)
-    content_tag(:div, class: 'error-label') do
-      "<i class='fa fa-close'></i> Oops. Something went wrong:".html_safe
-    end +
+    content_tag(:div, fa_icon('close', text: "That didn't work because:"), class: 'error-label') +
     content_tag(:ul, class: 'error-list') do
-      errors.full_messages.map { |message| content_tag(:li, message.gsub('Content', 'Message')) }.join.html_safe
+      errors.full_messages.map do |message|
+        next if message =~ /is invalid/
+        content_tag(:li, message.gsub('Content', 'Message'))
+      end.join.html_safe
     end
   end
 
@@ -19,4 +19,14 @@ module ApplicationHelper
     end).html_safe
   end
 
+  def external_link_to(options = {})
+    link_to options[:url], class: options[:html], title: options[:title], target: '_blank' do
+      icon(options[:icon], options[:text])
+    end
+  end
+
+  def icon(name, text)
+    return text unless name.present?
+    fa_icon(name, text: text)
+  end
 end
