a12017b121a7571e2b4db1243ee70f4bfc8aee2b
HunterMeyer <huntermeyer@live.com>
2017-01-14 18:54:54 -0500

    Remove reference, add image_url to books

--- db/migrate/20170114225140_create_books.rb
+++ db/migrate/20170114225140_create_books.rb
@@ -6,10 +6,10 @@ class CreateBooks < ActiveRecord::Migration
       t.text :genre
       t.text :subject
       t.text :summary
-      t.text :reference
       t.text :isbn
       t.text :status, default: 'Active'
       t.text :url
+      t.text :image_url
       t.integer :ordinal
       t.integer :clicks, default: 0
       t.datetime :published_at
