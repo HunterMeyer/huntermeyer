17c350bbf52e792b6992d4117b2e994fe52fc292
Hunter Meyer <huntermeyer@live.com>
2019-07-21 19:28:49 -0400

    Set up purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,18 @@
 module.exports = {
   plugins: [
+    require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-import'),
-    require('tailwindcss'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
+    }),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./app/**/*.html.erb'],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     })
   ]
 }
