75251b50f673531eca18ad7f5fe71169ff501965
Hunter Meyer <huntermeyer@live.com>
2019-07-21 19:15:34 -0400

    WIP Work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @work = Work.visible.order(:ordinal)
+  end
 end
---
+++ app/models/work.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Work < ApplicationRecord
+  scope :visible, -> { where(visible: true) }
+end
