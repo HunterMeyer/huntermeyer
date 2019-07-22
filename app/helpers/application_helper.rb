ec067f269fd8db36c06237d9042c7f25a542a7ca
Hunter Meyer <huntermeyer@live.com>
2019-07-21 22:02:00 -0400

    Home splash page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def external_link_to(options = {})
+    link_to options[:url], class: options[:html], title: options[:title], target: '_blank' do
+      content_tag(:i, options[:text], class: options[:icon])
+    end
+  end
 end
