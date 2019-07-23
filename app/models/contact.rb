46b343c91966a2bfadb2d96b26a2ef2348128920
Hunter Meyer <huntermeyer@live.com>
2019-07-23 12:22:01 -0400

    Add Contact and specs

---
+++ app/models/contact.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Contact < ApplicationRecord
+  validates :email, presence: true, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :content, presence: true
+  after_create { send_email }
+  before_save { email.downcase! }
+
+  def send_email
+    ContactMailer.new_contact(self).deliver
+  end
+end
