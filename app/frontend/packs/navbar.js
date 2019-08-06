03ba8d69d1c75166b9d56f4ad37cf4f3d9b1378e
Hunter Meyer <huntermeyer@live.com>
2019-08-06 01:44:29 -0400

    Move navbar script to own file

---
+++ app/frontend/packs/navbar.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  const navbar = $('#navbar');
+  const navbarHeight = navbar.height();
+  const splashHeight = $('#splash').height();
+  const navbarVisibileAt = splashHeight - navbarHeight;
+  navbar.hide();
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= navbarVisibileAt) {
+      navbar.show();
+    } else {
+      navbar.hide();
+    }
+  });
+});
