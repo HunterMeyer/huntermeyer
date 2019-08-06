257a4b8d7afe26c4e7d60a114486376c2396c2e2
Hunter Meyer <huntermeyer@live.com>
2019-08-06 01:57:39 -0400

    Only show navbar if sticky

--- app/frontend/packs/navbar.js
+++ app/frontend/packs/navbar.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
   const navbarHeight = navbar.height();
   const splashHeight = $('#splash').height();
   const navbarVisibileAt = splashHeight - navbarHeight;
-  navbar.hide();
-  $(window).scroll(function() {
-    if ($(window).scrollTop() >= navbarVisibileAt) {
-      navbar.show();
-    } else {
-      navbar.hide();
-    }
-  });
+  if (splashHeight) {
+    navbar.hide();
+    $(window).scroll(function() {
+      if ($(window).scrollTop() >= navbarVisibileAt) {
+        navbar.show();
+      } else {
+        navbar.hide();
+      }
+    });
+  }
 });
