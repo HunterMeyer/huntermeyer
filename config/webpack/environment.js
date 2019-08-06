8be0debc4ffc3c839903adc24bac03e8d8e479f9
Hunter Meyer <huntermeyer@live.com>
2019-08-06 01:42:49 -0400

    Add jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    JQuery: 'jquery',
+    jquery: 'jquery',
+  })
+)
+
+const config = environment.toWebpackConfig()
+
+config.resolve.alias = {
+  jquery: 'jquery/src/jquery'
+}
 
 module.exports = environment
